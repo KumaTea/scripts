2bdc7c1da911213cb4ed86150aae7757b2141a9d
KumaTea <KumaTea@outlook.com>
2023-11-30 05:55:28 +0800

    create init.sh

---
+++ linux/init.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -ex
+
+export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+
+# ssh stuff
+mkdir -p /root/.ssh
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINvrdbh3+SaWX5X12aRlPTrrx4ZDsOBvAo++cUKzwEUG root" >> /root/.ssh/authorized_keys
+touch /root/.hushlogin
+
+read -p "Add user kuma (y/N): " selection
+if [ "$selection" = "y" ]; then
+adduser --disabled-password --gecos "" kuma
+
+mkdir -p /home/kuma/.ssh
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRcdADho8lDItb6+3Q4qIyxGlL4Y4PkhcK6Yn0NJyLN kuma" >> /home/kuma/.ssh/authorized_keys
+touch /home/kuma/.hushlogin
+fi
+
+# sources.list
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+
+cat << EOF >> /etc/apt/sources.list
+deb https://mirrors.tuna.tsinghua.edu.cn/debian bookworm main contrib
+deb https://mirrors.tuna.tsinghua.edu.cn/debian bookworm-updates main contrib
+deb https://mirrors.tuna.tsinghua.edu.cn/debian bookworm-backports main contrib
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bookworm-security main contrib
+EOF
+
+# timezone
+ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
+
+# locales
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+echo 'LANG="en_US.UTF-8"'>/etc/default/locale
+dpkg-reconfigure --frontend=noninteractive locales
+update-locale LANG=en_US.UTF-8
